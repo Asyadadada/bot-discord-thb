2c3fc769480bd3d18f74a9d5d929301f3a3d0bb3
affaalfiandy <affaalfiandy@gmail.com>
2021-09-16 21:56:52 +0700

    testing bot

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello World')
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply('Helo Asu!')
+
+Token = #Token ada di channel dev bot
+
+bot.run(Token)
